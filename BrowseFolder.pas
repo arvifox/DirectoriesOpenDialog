221666b61a91bda4b5521fe68720930b9ac1d2bc
ArviFox <arvifox@hotmail.com>
2014-06-13 20:00:59 +0400

    add files of project

---
+++ BrowseFolder.pas
@@ -0,0 +1,89 @@
+unit BrowseFolder;
+
+interface
+
+uses
+  Windows, SysUtils, Classes, ShellAPI, ShlObj;
+
+type
+  TBrowseFolder = class(TComponent)
+  private
+    { Private declarations }
+  protected
+    { Protected declarations }
+  public
+    { Public declarations }
+    StartDir: string;
+    ResultDir: string;
+    TitleName: string;
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    function Execute(phwnd: HWND): boolean;
+  published
+    { Published declarations }
+
+  end;
+
+procedure Register;
+
+implementation
+
+procedure Register;
+begin
+  RegisterComponents('Samples', [TBrowseFolder]);
+end;
+
+{ TBrowseFolder }
+
+constructor TBrowseFolder.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+end;
+
+destructor TBrowseFolder.Destroy;
+begin
+  inherited Destroy;
+end;
+
+function BrowseCallbackProc(hwnd: HWND; uMsg: UINT; lParam: integer;
+                            lpData: integer): integer; stdcall;
+begin
+  // какое сообщение получили
+  if uMsg=BFFM_INITIALIZED then
+    // отправляем сообщение на изменение каталога
+    SendMessage(hwnd,BFFM_SETSELECTION,1,lpData);
+  result:=0;
+end;
+
+function TBrowseFolder.Execute(phwnd: HWND): boolean;
+var 
+  pp: string;
+  lpItemID : PItemIDList;
+  BrowseInfo : TBrowseInfo;
+  DisplayName : array[0..MAX_PATH] of char;
+  TempPath : array[0..MAX_PATH] of char;
+begin
+  result:=false;
+  FillChar(BrowseInfo, sizeof(TBrowseInfo), #0);
+  // инициализация
+  BrowseInfo.hwndOwner := phwnd;
+  BrowseInfo.pszDisplayName := @DisplayName;
+  BrowseInfo.lpszTitle := PChar(TitleName);
+  BrowseInfo.ulFlags := BIF_RETURNONLYFSDIRS + BIF_NEWDIALOGSTYLE;
+  BrowseInfo.lpfn:=@BrowseCallbackProc;
+  pp:=StartDir;
+  lstrcpy(TempPath,@pp[1]);
+  BrowseInfo.lParam:=integer(@TempPath);
+  // показ диалога
+  lpItemID := SHBrowseForFolder(BrowseInfo);
+  if lpItemId <> nil then
+  begin
+    SHGetPathFromIDList(lpItemID, TempPath);
+    SetLength(ResultDir,lstrlen(TempPath));
+    lstrcpy(@ResultDir[1],TempPath);
+    GlobalFreePtr(lpItemID); 
+    result:=true;
+  end;
+end;
+
+end.
